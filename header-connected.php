a40679487cb3c6a502f872d5de92f4e436d6a75d
florian.llimos <florian.llimos@digitanie.org>
2024-11-25 16:41:41 +0100

    feat: create header for user connected

---
+++ header-connected.php
@@ -0,0 +1,16 @@
+<header class="header">
+  <nav class="navbar container">
+    <h1><span class="red">S</span>ocial <span class="red">C</span>onnect</h1>
+    <input type="checkbox" id="toggler" />
+    <label for="toggler"><img src="media/burger.png" class="burgerLogo"></img></label>
+    <div class="menu">
+      <ul class="list">
+        <a href="fil-actualite.php"><li class="LiNavTop"><img src="media/accueil.png" alt="Logo de compte" class="logo"></li></a>
+        <a href=" conversation-prive.php"><li class="LiNavTop"><img src="media/messager.png" alt="Logo de compte" class="logo"></li></a>
+        <a href="mon-compte.php"><li class="LiNavTop"><img src="media/user.png" alt="Logo de compte" class="logo"></li></a>
+        <a href="publications.php"><li class="LiNavTop"><img src="media/un-journal.png" alt="Logo de publication" class="logo"></li></a>
+        <a href="composants/deconnexion.php"><li class="LiNavTop"><img src="media/deconnexion.png" alt="Logo de déconnexion" class="logo"></li></a>
+      </ul>
+    </div>
+  </nav>
+</header>
