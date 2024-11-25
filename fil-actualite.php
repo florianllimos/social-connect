c17462ca4f5524ce8358534ae7d662b622a219cb
florian.llimos <florian.llimos@digitanie.org>
2024-11-25 16:43:31 +0100

    feat: user interface for the feed

---
+++ fil-actualite.php
@@ -0,0 +1,13 @@
+<?php
+
+require_once("composants/user-connected.php");
+
+require_once("composants/header.php");
+
+require_once("composants/database.php");
+
+require_once("list-account.php");
+
+require_once("header-connected.php");
+
+require_once("composants/container-publication.php");
